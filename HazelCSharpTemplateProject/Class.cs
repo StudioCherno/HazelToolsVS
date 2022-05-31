c567039444350f4f834fd0d2549fca6c91ae9a65
Peter Nilsson <peter@studiocherno.com>
2022-05-31 19:09:14 +0200

    Added custom C# class template for Hazel Entity scripts

---
+++ HazelCSharpTemplateProject/Class.cs
@@ -0,0 +1,24 @@
+﻿using Hazel;
+using System;
+
+namespace $rootnamespace$
+{
+	public class $safeitemrootname$ : Entity
+	{
+		
+		/// <summary>
+		/// OnCreate is called once when the Entity that this script is attached to
+		/// is instantiated in the scene at runtime
+		/// </summary>
+		protected override void OnCreate()
+		{
+		}
+
+		/// <summary>
+		/// OnUpdate is called once every frame while this script is active in the scene
+		/// </summary>
+		protected override void OnUpdate(float deltaTime)
+		{
+		}
+	}
+}
---
+++ HazelCSharpTemplateProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hazel Visual Studio Templates Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Studio Cherno")]
+[assembly: AssemblyProduct("HazelCSharpTemplateProject")]
+[assembly: AssemblyCopyright("Copyright © Studio Cherno 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("46abafd5-dc8a-4441-82dd-4ad3928b6845")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
